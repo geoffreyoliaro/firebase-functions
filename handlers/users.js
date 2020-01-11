79f1c4ecf7fb4772264447cb0b950b6980fd9093
szeth <geoffreyoliaro@gmail.com>
2020-01-11 10:20:40 +0300

    some changes in validations

--- handlers/users.js
+++ handlers/users.js
@@ -41,7 +41,7 @@ exports.signup = (req,res)=>{
             handle: newUser.handle,
             email:newUser.email,
             createdAt:new Date().toISOString(),
-            imageUrl:`https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/0/${noImg}?alt=media`,
+            imageUrl:`https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
             userId
         };
        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
--- util/validators.js
+++ util/validators.js
@@ -1,12 +1,13 @@
-const isEmpty =(String) =>{
-    if(String.trim()==='')return true;
-    else return false;
-};
 const isEmail = (email)=>{
     const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(email.match(regEx)) return true;
     else return false;
 }
+
+const isEmpty =(String) =>{
+    if(String.trim()==="")return true;
+    else return false;
+};
 
 exports.validateSignupData = (data) =>{
     let errors = {};
